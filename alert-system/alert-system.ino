ccfce86a6f63e3e739a4115efde8a05755a259cc
mkceliks <66789068+mkceliks@users.noreply.github.com>
2022-04-10 17:37:31 +0300

    alert-system v2.0
    Button added to the system. If the user clicks the button when the alert becames type of too dangerous, yellow LED becomes 'HIGH' and the process continues until it is restored.

--- alert-system/alert-system.ino
+++ alert-system/alert-system.ino
@@ -5,8 +5,10 @@ DHT dht(DHTPIN, DHT11);  // dht adında sensör nesnesi tanımla.
 #define LedYellow 9
 #define LedGreen 3
 #define potPin A0
+#define button 2
 int potDegeri = 0;
 int mapValue = 0;
+int buttonStatus = 0;
 void setup() {
   Serial.begin(9600);  
   dht.begin();  //dht sensörü başlat
@@ -14,22 +16,53 @@ void setup() {
   pinMode(LedYellow,OUTPUT);
   pinMode(LedRed,OUTPUT);
   pinMode(potPin,INPUT);
+  pinMode(button,INPUT);
   
 }
 
 void loop() {
+  
   float nem = dht.readHumidity(); 
-  float sicaklik = dht.readTemperature();  
-  Serial.print("Nem: Yüzde ");      
-  Serial.print(nem  );
+  float sicaklik = dht.readTemperature(); 
+  Serial.println(" HAVA DURUMU ");
+  Serial.print(" Nem: % ");      
+  Serial.println(nem  );
   Serial.print(" Sıcaklık: ");
   Serial.print(sicaklik);
-  Serial.print(" C");
+  Serial.println("°C");
+  
   mapValue = analogRead(potPin);
   potDegeri=map(mapValue,0,1023,0,255);
   Serial.print(" POTANSİYEL METRE: ");
   Serial.println(potDegeri);
+  
+  buttonStatus = digitalRead(button);
+  Serial.println(" Alarm Buton = DEAKTİF");
+  Serial.println("*************************************************");
+     if(buttonStatus == 1){
+      Serial.println("ALARMA BASILDI!!!!!!");
+      delay(2000);
+      while(nem > 60){
+      Serial.println("İŞLEM KONTROL ALTINA ALINIYOR...");
+      delay(2000);
+      float nem = dht.readHumidity(); 
+      Serial.print("Anlık Nem: Yüzde ");      
+      Serial.println(nem);
+      if(nem < 60){
+        break;
+      }
+      analogWrite(LedYellow,potDegeri);
+      digitalWrite(LedRed,LOW);
+      digitalWrite(LedGreen,LOW);
+      }
+      Serial.println("İŞLEM KONTROL ALTINA BAŞARIYLA ALINDI... ");
+      delay(2000);
+      buttonStatus = 0;
+  }
+    nem = dht.readHumidity(); 
     if(nem >= 70){
+    Serial.println("ÇOK TEHLİKELİ SEVİYEDE!!!!!!");
+    delay(500);
     analogWrite(LedRed,potDegeri);
     digitalWrite(LedYellow,LOW);
     digitalWrite(LedGreen,LOW);
@@ -38,6 +71,7 @@ void loop() {
     delay(100);
   }
   else if(nem >= 65){
+    Serial.println("TEHLİKELİ SEVİYEDE!!!!!!");
     analogWrite(LedRed,potDegeri);
     digitalWrite(LedYellow,LOW);
     digitalWrite(LedGreen,LOW);
@@ -45,7 +79,8 @@ void loop() {
     digitalWrite(LedRed,LOW);
     delay(300);
   }
-  else if(nem >=60){
+  else if(nem >60){
+    Serial.println("TEHLİKELİ OLABİLİR!!!!!!");
     analogWrite(LedRed,potDegeri);
     digitalWrite(LedYellow,LOW);
     digitalWrite(LedGreen,LOW);
@@ -57,5 +92,8 @@ else{
     analogWrite(LedGreen,potDegeri);
     digitalWrite(LedYellow,LOW);
     digitalWrite(LedRed,LOW);
+    delay(3000);
   }
-}
+  
+  }
+    
